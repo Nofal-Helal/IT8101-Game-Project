b41d15cf6cb0de039ac3f0f451cea79a5b4b2302
zia-zedia <ziagallaf578@gmail.com>
2023-11-23 00:22:28 +0300

    Added the main menu

---
+++ Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public void goToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
