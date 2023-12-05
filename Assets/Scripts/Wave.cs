af7b2996336d15bf2ed11fc2fc04b1f729847774
Nofal-Helal <202001264@student.polytechnic.bh>
2023-12-05 20:50:42 +0300

    Raycast enemy placement in waves
    Place wave spawned enemies on the ground by using raycasting

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -26,7 +26,10 @@ public class Wave : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        if (obstacle == null) Debug.LogError("Wave has no Obstacle attached");
+        if (obstacle == null)
+        {
+            Debug.LogError("Wave has no Obstacle attached");
+        }
 
         collider = GetComponent<BoxCollider>();
 
@@ -35,23 +38,48 @@ public class Wave : MonoBehaviour
 
     private void SpawnWave()
     {
-        Debug.Log("Spawning Wave");
-
-        foreach (var enemySpawns in enemies)
+        foreach (WaveEnemies enemySpawns in enemies)
         {
             int spawned = 0;
             while (spawned < enemySpawns.count)
             {
-                Vector3 position = new Vector3(
-                    Random.Range(collider.bounds.min.x, collider.bounds.max.x),
-                    Random.Range(collider.bounds.min.y, collider.bounds.max.y),
-                    Random.Range(collider.bounds.min.z, collider.bounds.max.z)
-                );
-                Instantiate(enemySpawns.enemy, position, Quaternion.identity);
-                spawned += 1;
-                Debug.Log("spawend " + spawned);
+                Vector3 initialPosition =
+                    new(
+                        Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+                        Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                        Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+                    );
+
+                Vector3? position = PlaceEnemy(initialPosition);
+                if (position.HasValue)
+                {
+                    _ = Instantiate(enemySpawns.enemy, position.Value, Quaternion.identity);
+                    spawned += 1;
+                }
             }
         }
+    }
+
+    /// <summary>
+    /// Places an enemy on the ground by ray tracing from an initial spawning
+    /// position.
+    /// </summary>
+    /// <param name="initialPosition">Initial position of spawning</param>
+    /// <returns>
+    /// Position on the ground or null if somehow the ray didn't hit
+    /// </returns>
+    private Vector3? PlaceEnemy(Vector3 initialPosition)
+    {
+        const int layerMask = 1 << 7;
+        return Physics.Raycast(
+            initialPosition,
+            Vector3.down,
+            out RaycastHit raycastHit,
+            10f,
+            layerMask
+        )
+            ? raycastHit.point
+            : null;
     }
 
     // Update is called once per frame
