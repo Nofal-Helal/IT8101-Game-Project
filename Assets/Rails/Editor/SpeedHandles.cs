7da5689a3fc6abf6361386f579db73a5b76150d9
Nofal-Helal <202001264@student.polytechnic.bh>
2023-12-11 20:39:53 +0300

    Add visual indicators for track speed tool

--- Assets/Rails/Editor/SpeedHandles.cs
+++ Assets/Rails/Editor/SpeedHandles.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEditor.Splines;
-using UnityEditor;
-using UnityEngine.Splines;
 using UnityEngine;
+using UnityEngine.Splines;
 
 [Icon("Packages/com.unity.splines/Editor/Resources/Icons/ToolHandleParent.png")]
 [EditorTool("Track Speed Tool", typeof(SplineContainer))]
@@ -11,6 +13,8 @@ public class SpeedHandles : EditorTool
 {
     Spline spline;
     SplineData<float> speedData;
+
+    GUIStyle style;
 
     void OnEnable()
     {
@@ -20,11 +24,38 @@ public class SpeedHandles : EditorTool
             speedData = spline.GetOrCreateFloatData("speed");
         }
         catch (Exception) { }
+
+        style = new();
+        style.normal.textColor = Color.black;
+        style.fontStyle = FontStyle.Bold;
+        style.fontSize = 16;
     }
 
     public override void OnToolGUI(EditorWindow _)
     {
         SplineDataHandles.DataPointHandles(spline, speedData);
+
+        // Display labels above each data point
+        foreach (
+            var dataPoint in speedData.Select(
+                (dp, i) =>
+                    new
+                    {
+                        Distance = dp.Index,
+                        Speed = dp.Value,
+                        Index = i
+                    }
+            )
+        )
+        {
+            float t = spline.ConvertIndexUnit(
+                dataPoint.Distance,
+                PathIndexUnit.Distance,
+                PathIndexUnit.Normalized
+            );
+            float3 position = spline.EvaluatePosition(t);
+            position.y += 1f;
+            Handles.Label(position, $"[{dataPoint.Index}] = {dataPoint.Speed}", style);
+        }
     }
 }
-
