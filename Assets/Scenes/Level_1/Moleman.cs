23f6e4e3e4bb3139b093d8c14a1cbb40d847baf3
Nofal-Helal <202001264@student.polytechnic.bh>
2024-01-10 18:02:24 +0300

    Moleman ranged attack

--- Assets/Scenes/Level_1/Moleman.cs
+++ Assets/Scenes/Level_1/Moleman.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Assertions;
@@ -20,7 +21,9 @@ public class Moleman : MonoBehaviour, IDamageTaker
     public float meleeAttackRange = 5.7f;
     public float jumpSpeed = 20f;
     [Range(0, 1)]
-    public float arc = 0.0f;
+    public float jumpArc = 0.0f;
+    public float projectileSpeed = 13f;
+    public float projectileUp = 1.5f;
     public float cooldown = 2;
     public float attackCooldown = 2.2f;
     public float dodgeCooldown = 2;
@@ -37,6 +40,8 @@ public class Moleman : MonoBehaviour, IDamageTaker
     public Transform spot2;
     public Transform spot3;
     public int inSpot = 1;
+    public Transform hand;
+    public GameObject projectilePrefab;
 
     [NaughtyAttributes.Button] void ChangeStateRanged() { ChangeState(State.RangedAttack); }
     [NaughtyAttributes.Button] void ChangeStateMelee() { ChangeState(State.MeleeAttack); }
@@ -113,8 +118,7 @@ public class Moleman : MonoBehaviour, IDamageTaker
         if (cooldown <= 0)
         {
             animator.SetTrigger("Throw");
-            // TODO: throw rock
-            // Debug.Log("HYaaaaaaaah");
+            StartCoroutine(ThrowRock());
             cooldown = attackCooldown;
         }
         cooldown -= Time.deltaTime;
@@ -122,7 +126,7 @@ public class Moleman : MonoBehaviour, IDamageTaker
         if (timeInState >= 3 * attackCooldown && cooldown > 0)
         {
             animator.ResetTrigger("Throw");
-            if (Random.Range(0,1f) < 0.5f)
+            if (Random.Range(0, 1f) < 0.5f)
             {
                 ChangeState(State.MeleeAttack);
             }
@@ -197,7 +201,7 @@ public class Moleman : MonoBehaviour, IDamageTaker
         rigidbody.drag = 0; // set drag to 0 to not resist the force
         yield return new WaitForSeconds(0.84444444f); // wait for jump in animation
         navMeshAgent.enabled = false; // disable nav agent to not interfere with physics
-        float time = rigidbody.ApplyTargetedForce(spot1.position, jumpSpeed*3f/2f, arc);
+        float time = rigidbody.ApplyTargetedForce(spot1.position, jumpSpeed * 3f / 2f, jumpArc);
         yield return new WaitForSeconds(time); // wait until jump ends 
         rigidbody.drag = 5f; // apply drag for friction
         navMeshAgent.enabled = true;
@@ -210,7 +214,7 @@ public class Moleman : MonoBehaviour, IDamageTaker
         rigidbody.drag = 0; // set drag to 0 to not resist the force
         yield return new WaitForSeconds(0.8444444f); // wait for jump in animation
         navMeshAgent.enabled = false; // disable nav agent to not interfere with physics
-        float time = rigidbody.ApplyTargetedForce(spot.position, jumpSpeed, arc);
+        float time = rigidbody.ApplyTargetedForce(spot.position, jumpSpeed, jumpArc);
         yield return new WaitForSeconds(time); // wait until jump ends 
         rigidbody.drag = 8f; // apply drag for friction
         navMeshAgent.enabled = true;
@@ -233,6 +237,17 @@ public class Moleman : MonoBehaviour, IDamageTaker
         {
             ChangeState(State.MeleeAttack);
         }
+    }
+
+    IEnumerator ThrowRock()
+    {
+        ThrownGround rock = Instantiate(projectilePrefab, hand).GetComponent<ThrownGround>();
+        rock.transform.localPosition = new Vector3(-0.662999988f, -0.0309999995f, 0.0970000029f);
+        yield return new WaitForSeconds(24 / 30f);
+        hand.DetachChildren();
+        rock.Throw(cart.position + new Vector3(0, 2.5f, 0), projectileSpeed, 0, projectileUp);
+        yield return new WaitForSeconds(4f);
+        if (rock != null) Destroy(rock.gameObject); // just in case
     }
 
     void IDamageTaker.TakeDamage(float damage)
@@ -259,7 +274,7 @@ static class RigidbodyExtension
     /// Applies an impulse that will have the rigidbody land at the specified target. The arch is the percent 
     /// of arch to provide between the min/max (0 to 1).  
     /// </summary>
-    public static float ApplyTargetedForce(this Rigidbody rRigidBody, Vector3 rTarget, float rSpeed, float rArc = 0.5f, bool rUseMinSpeed = true)
+    public static float ApplyTargetedForce(this Rigidbody rRigidBody, Vector3 rTarget, float rSpeed, float rArc = 0.5f, float extraUpTime = 0f, bool rUseMinSpeed = true)
     {
         float lGravity = Physics.gravity.magnitude;
         Vector3 lObjectPosition = rRigidBody.position;
@@ -287,7 +302,7 @@ static class RigidbodyExtension
         // Determine the force based on our arc value
         Mathf.Clamp(rArc, 0f, 1f);
         float lTime = lMinTime + ((lMaxTime - lMinTime) * rArc);
-        Vector3 lForce = new Vector3(lToTarget.x / lTime, (lToTarget.y / lTime) + (lTime * lGravity / 2f), lToTarget.z / lTime);
+        Vector3 lForce = new Vector3(lToTarget.x / lTime, (lToTarget.y / lTime) + ((lTime + extraUpTime) * lGravity / 2f), lToTarget.z / lTime);
 
         // Apply the force
         rRigidBody.AddForce(lForce, ForceMode.Impulse);
